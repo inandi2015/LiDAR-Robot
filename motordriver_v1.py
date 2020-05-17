731c6d4fc513a3c2e9a105306d6f548d95972d7f
Zihao Xue <zx278@cornell.edu>
2020-05-17 14:41:42 -0400

    Robot class

--- motordriver_v1.py
+++ motordriver_v1.py
@@ -9,6 +9,11 @@ class MotorController:
 		self.controller = pigpio.pi() # access the local pi's GPIO
 		# GPIO22 GPIO23 GPIO24 GPIO25
 		# 1 0 1 0
+		self.lastPWM1 = 0
+		self.lastPWM2 = 0
+		self.left_speed_table = [()]*80
+		self.right_speed_table = [()]*80
+		self.fill_speed_tables()
 		self.controller.write(22, 1)
 		self.controller.write(23, 0)
 		self.controller.write(24, 1)
@@ -17,32 +22,80 @@ class MotorController:
 		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 0) 
 		
 	def forward(self):
-		self.controller.hardware_PWM(13, WORKING_FREQUENCY, 350000) 
-		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 350000) 
+		if(self.lastPWM1 == 300000 and self.lastPWM2 == 300000):
+			return
+		self.controller.hardware_PWM(13, WORKING_FREQUENCY, 300000) 
+		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 300000) 
+		self.lastPWM1, self.lastPWM2 = 300000, 300000
 	
 	def stop(self):
+		if(self.lastPWM1 == 0 and self.lastPWM2 == 0):
+			return
 		self.controller.hardware_PWM(13, WORKING_FREQUENCY, 0) 
 		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 0)
+		self.lastPWM1, self.lastPWM2 = 0, 0
 	
-	def turn_left(self):
-		self.controller.hardware_PWM(13, WORKING_FREQUENCY, 50000) 
-		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 600000) 
+	def turn_left(self, index = None):
+		current_PWM1, current_PWM2 = 0, 0
+		if not index:
+			current_PWM1, current_PWM2 = 50000, 800000 # front too close
+		else:
+			current_PWM1, current_PWM2 = self.right_speed_table[index]
+		if(self.lastPWM1 == current_PWM1 and self.lastPWM2 == current_PWM2):
+			return
+		self.controller.hardware_PWM(13, WORKING_FREQUENCY, current_PWM1) 
+		self.controller.hardware_PWM(12, WORKING_FREQUENCY, current_PWM2) 
+		self.lastPWM1, self.lastPWM2 = current_PWM1, current_PWM2
 	
-	def turn_right(self):
-		self.controller.hardware_PWM(13, WORKING_FREQUENCY, 600000) 
-		self.controller.hardware_PWM(12, WORKING_FREQUENCY, 50000) 
+	def turn_right(self, index = None):
+		current_PWM1, current_PWM2 = 0, 0
+		if not index:
+			current_PWM1, current_PWM2 = 800000, 50000 # front too close
+		else:
+			current_PWM1, current_PWM2 = self.left_speed_table[index]
+		if(self.lastPWM1 == current_PWM1 and self.lastPWM2 == current_PWM2):
+			return
+		self.controller.hardware_PWM(13, WORKING_FREQUENCY, current_PWM1) 
+		self.controller.hardware_PWM(12, WORKING_FREQUENCY, current_PWM2) 
+		self.lastPWM1, self.lastPWM2 = current_PWM1, current_PWM2
 		
+	def fill_speed_tables(self):
+		for i in range(10):
+			self.left_speed_table[i] = (700000, 20000)
+			self.right_speed_table[i] = (200000, 350000)
+		for i in range(10, 20):
+			self.left_speed_table[i] = (680000, 25000)
+			self.right_speed_table[i] = (100000, 400000)
+		for i in range(20, 30):
+			self.left_speed_table[i] = (650000, 50000)
+			self.right_speed_table[i] = (80000, 450000)
+		for i in range(30, 40):
+			self.left_speed_table[i] = (600000, 60000)
+			self.right_speed_table[i] = (70000, 500000)
+		for i in range(40, 50):
+			self.left_speed_table[i] = (500000, 70000)
+			self.right_speed_table[i] = (60000, 600000)
+		for i in range(50, 60):
+			self.left_speed_table[i] = (450000, 80000)
+			self.right_speed_table[i] = (50000, 650000)
+		for i in range(60, 70):
+			self.left_speed_table[i] = (400000, 100000)
+			self.right_speed_table[i] = (25000, 680000)
+		for i in range(70, 80):
+			self.left_speed_table[i] = (350000, 200000)
+			self.right_speed_table[i] = (20000, 700000)
 
 if __name__ == "__main__":
 	import time
 	Controller = MotorController()
+	#print(len(Controller.left_speed_table))
 	#print(Controller.controller.read(22), Controller.controller.read(23), 
 	# Controller.controller.read(24), Controller.controller.read(25))
-	Controller.forward()
-	time.sleep(2)
-	Controller.turn_left()
-	time.sleep(2)
-	Controller.turn_right()
-	time.sleep(2)
-	Controller.stop()
+	#Controller.forward()
+	#time.sleep(2)
+	#Controller.turn_left()
+	#time.sleep(2)
+	#Controller.turn_right()
+	#time.sleep(2)
+	#Controller.stop()
 
---
+++ robot_v1.py
@@ -0,0 +1,50 @@
+import numpy as np
+from lidar_data import lidarReader
+from motordriver_v1 import MotorController
+import threading
+import time
+
+class Robot:
+	def __init__(self):
+		self.lidar = lidarReader('/dev/mylidar', 115200)
+		self.mover = MotorController()
+		self.left_distance_table = np.zeros(80)
+		self.right_distance_table = np.zeros(80)
+		self.thread = threading.Thread(target=self.navigate, args=())
+		self.thread.start()
+		
+	def navigate(self):
+		while True:
+			time.sleep(0.1) # (yield) allowing reading data from the serailport
+			front_too_close, left_too_close, right_too_close = False, False, False
+			if(self.lidar.angle_180 < 400):
+				front_too_close = True
+			left_angle = np.argmin(self.lidar.left_container)
+			right_angle = np.argmin(self.lidar.right_container)
+			if(self.lidar.left_container[left_angle] < 250):
+				left_too_close = True
+			if(self.lidar.right_container[right_angle] < 250):
+				right_too_close = True
+			if(front_too_close and left_too_close and right_too_close):
+				self.mover.stop()
+			elif(front_too_close):
+				if(self.lidar.angle_270 > self.lidar.angle_90):
+					self.mover.turn_left()
+				else:
+					self.mover.turn_right()
+			elif(left_too_close or right_too_close):
+				if(left_too_close):
+					self.mover.turn_right(left_angle)
+				else:
+					self.mover.turn_left(right_angle)
+			else:
+				self.mover.forward()
+				
+			#print(front_too_close, left_too_close, right_too_close)
+			
+			
+
+if __name__ == "__main__":
+	robot = Robot()
+	while True:
+		time.sleep(10)
